88992db3d8d9f50597571e3407bf5e9515e6e959
Riccardo Rimpici <133099369+rimpx@users.noreply.github.com>
2025-04-09 17:26:34 +0000

    modifica host vue

--- appvue/worldflighttracker/vue.config.js
+++ appvue/worldflighttracker/vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  devServer: {
+    host: '0.0.0.0',         // Permette accessi esterni (non solo localhost)
+    port: 8080,              // La porta che usi nel tuo Dockerfile
+    disableHostCheck: true   // Disattiva il controllo dell'host (risolve "Invalid Host header")
+  }
 })
