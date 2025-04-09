211e98331c6c5959e2c416033bdf59177bc213a8
Riccardo Rimpici <133099369+rimpx@users.noreply.github.com>
2025-04-09 17:55:11 +0000

    rollback

--- appvue/worldflighttracker/vue.config.js
+++ appvue/worldflighttracker/vue.config.js
@@ -1,10 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-
-  devServer: {
-    host: '0.0.0.0',
-    port: 8080,
-    allowedHosts: 'all', // ← questa è la nuova alternativa a disableHostCheck
-  }
+  transpileDependencies: true
 })
