a3ee0c65ca31a823dfcb61b37bbcb56409b557dd
Riccardo Rimpici <133099369+rimpx@users.noreply.github.com>
2025-04-09 17:47:44 +0000

    host 2

--- appvue/worldflighttracker/vue.config.js
+++ appvue/worldflighttracker/vue.config.js
@@ -3,8 +3,8 @@ module.exports = defineConfig({
   transpileDependencies: true,
 
   devServer: {
-    host: '0.0.0.0',         // Permette accessi esterni (non solo localhost)
-    port: 8080,              // La porta che usi nel tuo Dockerfile
-    disableHostCheck: true   // Disattiva il controllo dell'host (risolve "Invalid Host header")
+    host: '0.0.0.0',
+    port: 8080,
+    allowedHosts: 'all', // ← questa è la nuova alternativa a disableHostCheck
   }
 })
